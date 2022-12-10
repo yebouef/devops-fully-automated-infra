11765a28be89fd4d08f36790cc3fa64761e0a02f
Franck Yeboue <yebouef@icloud.com>
2022-12-10 14:57:10 -0500

    code improvements 3

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["146.70.93.204/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
